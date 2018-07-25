aa0058d7be10c146094bb72ea9f090a2a6c6e4cd
xiaonan32 <732879625@qq.com>
2018-07-25 18:26:42 +0800

    初始构建中

--- app/src/main/java/com/gjn/gamequery/ui/MainActivity.java
+++ app/src/main/java/com/gjn/gamequery/ui/MainActivity.java
@@ -1,15 +1,22 @@
 package com.gjn.gamequery.ui;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import com.gjn.gamequery.R;
+import com.gjn.gamequery.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
     }
 }
--- app/src/main/java/com/gjn/gamequery/ui/splash/SplashActivity.java
+++ app/src/main/java/com/gjn/gamequery/ui/splash/SplashActivity.java
@@ -1,9 +1,40 @@
 package com.gjn.gamequery.ui.splash;
 
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.gjn.gamequery.R;
+import com.gjn.gamequery.base.BaseActivity;
+import com.gjn.gamequery.ui.MainActivity;
+
 /**
  * @author gjn
  * @time 2018/7/25 17:35
  */
 
-public class SplashActivity {
+public class SplashActivity extends BaseActivity{
+
+    private String url = "https://ww1.sinaimg.cn/large/0065oQSqly1ftf1snjrjuj30se10r1kx.jpg";
+    private ImageView imageView;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_splash;
+    }
+
+    @Override
+    protected void initView() {
+        imageView = findViewById(R.id.iv_splash);
+        Glide.with(mActivity).load(url).into(imageView);
+    }
+
+    @Override
+    protected void initData() {
+        imageView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                showNextActivity(MainActivity.class);
+            }
+        }, 5000);
+    }
 }
